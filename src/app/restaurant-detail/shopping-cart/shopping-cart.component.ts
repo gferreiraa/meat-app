3d9188aa519f375bf7d1e125a3cf399e4c7c75ab
gferreira <gferreiraa@gmail.com>
2019-01-10 23:11:17 -0200

    Apply methods shoppingCart

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -16,6 +16,18 @@ export class ShoppingCartComponent implements OnInit {
     return this.shoppingCartService.items;
   }
 
+  clear() {
+    this.shoppingCartService.clear();
+  }
+
+  removeItem(item: any) {
+    this.shoppingCartService.removeItem(item);
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item);
+  }
+
   total(): number {
     return this.shoppingCartService.total();
   }
