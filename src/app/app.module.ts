c4b4e5a29725ff37b015b34270d8605bd5b2a43d
gferreira <gferreiraa@gmail.com>
2019-01-10 22:50:10 -0200

    Created shoppingCart Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 
 
 @NgModule({
@@ -38,7 +39,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [ RestaurantService ],
+  providers: [ RestaurantService, ShoppingCartService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
