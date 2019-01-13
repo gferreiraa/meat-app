847027aa8c442de244cec32f7eda739762f4c079
gferreira <gferreiraa@gmail.com>
2019-01-13 14:08:13 -0200

    Created Create Value Acessor

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,23 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option.model';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
 export class RadioComponent implements OnInit {
 
   @Input() options: RadioOption[];
 
   value: any;
+  onChange: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  setValue(value: any){
-    this.value = value
+  setValue(value: any) {
+    this.value = value;
+    this.onChange(this.value);
   }
 
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
 }
