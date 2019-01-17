fd06ac84449fdd8784898e6eb1fb649fce54c912
gferreira <gferreiraa@gmail.com>
2019-01-17 02:17:10 -0200

    Finish animations

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,15 +1,26 @@
 import { MenuItem } from './menu-item.model';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
   // Sempre que você tiver uma propriedade que o component parent vai informar para você, você precisa utiliår o @Input()
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter();
+
+  menuItemState = 'ready';
 
   constructor() { }
 
