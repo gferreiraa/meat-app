c1f4f5c613839c11851f288167af8ca6ef86ae67
gferreira <gferreiraa@gmail.com>
2019-01-19 23:41:10 -0200

    Created search-bar

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 import { RestaurantService } from './restaurants.services';
 import { Restaurant } from './restaurant/restaurant.model';
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
+
+  searchBarState = 'hidden';
 
   restaurants: Restaurant[];
 
@@ -15,6 +32,10 @@ export class RestaurantsComponent implements OnInit {
   ngOnInit() {
     this.restaurantsService.restaurants()
       .subscribe( restaurants => this.restaurants = restaurants);
+  }
+
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 
 }
